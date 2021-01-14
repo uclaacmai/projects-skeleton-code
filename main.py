7f339f6b2cbf2d9c5ab3a17614918957980c86fe
Frank Zheng <ztcfrank123@gmail.com>
2021-01-13 21:14:51 -0800

    Created skeleton code

---
+++ main.py
@@ -0,0 +1,57 @@
+import argparse
+import os
+import time
+
+
+import constants
+from datasets.StartingDataset import StartingDataset
+from networks.StartingNetwork import StartingNetwork
+from train_functions.starting_train import starting_train
+
+
+SUMMARIES_PATH = "training_summaries"
+
+
+def main():
+    # Get command line arguments
+    args = parse_arguments()
+    hyperparameters = {"epochs": args.epochs, "batch_size": args.batch_size}
+
+    # Create path for training summaries
+    label = f"cassava__{int(time.time())}"
+    summary_path = f"{SUMMARIES_PATH}/{label}"
+    os.makedirs(summary_path, exist_ok=True)
+
+    # TODO: Add GPU support. This line of code might be helpful.
+    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    print("Summary path:", summary_path)
+    print("Epochs:", args.epochs)
+    print("Batch size:", args.batch_size)
+
+    # Initalize dataset and model. Then train the model!
+    train_dataset = StartingDataset()
+    val_dataset = StartingDataset()
+    model = StartingNetwork()
+    starting_train(
+        train_dataset=train_dataset,
+        val_dataset=val_dataset,
+        model=model,
+        hyperparameters=hyperparameters,
+        n_eval=args.n_eval,
+        summary_path=summary_path,
+    )
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--epochs", type=int, default=constants.EPOCHS)
+    parser.add_argument("--batch_size", type=int, default=constants.BATCH_SIZE)
+    parser.add_argument(
+        "--n_eval", type=int, default=constants.N_EVAL,
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
---
+++ train_functions/starting_train.py
@@ -0,0 +1,90 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.tensorboard
+
+
+def starting_train(
+    train_dataset, val_dataset, model, hyperparameters, n_eval, summary_path
+):
+    """
+    Trains and evaluates a model.
+
+    Args:
+        train_dataset:   PyTorch dataset containing training data.
+        val_dataset:     PyTorch dataset containing validation data.
+        model:           PyTorch model to be trained.
+        hyperparameters: Dictionary containing hyperparameters.
+        n_eval:          Interval at which we evaluate our model.
+        summary_path:    Path where Tensorboard summaries are located.
+    """
+
+    # Get keyword arguments
+    batch_size, epochs = hyperparameters["batch_size"], hyperparameters["epochs"]
+
+    # Initialize dataloaders
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=True
+    )
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=True
+    )
+
+    # Initalize optimizer (for gradient descent) and loss function
+    optimizer = optim.Adam(model.parameters())
+    loss_fn = nn.CrossEntropyLoss()
+
+    # Initialize summary writer (for logging)
+    writer = torch.utils.tensorboard.SummaryWriter(summary_path)
+
+    step = 0
+    for epoch in range(epochs):
+        print(f"Epoch {epoch + 1} of {epochs}")
+
+        # Loop over each batch in the dataset
+        for i, batch in enumerate(train_loader):
+            print(f"\rIteration {i + 1} of {len(train_loader)} ...", end="")
+
+            # TODO: Backpropagation and gradient descent
+
+            # Periodically evaluate our model + log to Tensorboard
+            if step % n_eval == 0:
+                # TODO:
+                # Compute training loss and accuracy.
+                # Log the results to Tensorboard.
+
+                # TODO:
+                # Compute validation loss and accuracy.
+                # Log the results to Tensorboard.
+                # Don't forget to turn off gradient calculations!
+                evaluate(val_loader, model, loss_fn)
+
+            step += 1
+
+        print()
+
+
+def compute_accuracy(outputs, labels):
+    """
+    Computes the accuracy of a model's predictions.
+
+    Example input:
+        outputs: [0.7, 0.9, 0.3, 0.2]
+        labels:  [1, 1, 0, 1]
+
+    Example output:
+        0.75
+    """
+
+    n_correct = (torch.round(outputs) == labels).sum().item()
+    n_total = len(outputs)
+    return n_correct / n_total
+
+
+def evaluate(val_loader, model, loss_fn):
+    """
+    Computes the loss and accuracy of a model on the validation dataset.
+
+    TODO!
+    """
+    pass
