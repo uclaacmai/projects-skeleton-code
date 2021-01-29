c26b4505ee6556590ccc00b7bb52a67babd02938
Advaith G <67539191+advaithg@users.noreply.github.com>
2021-01-29 10:57:57 +0530

    Changed startingdataset, added cnn

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -1,29 +1,33 @@
 import torch
+import os
+from skimage import io
 import pandas as pd
 import matplotlib.pyplot as plt
-from PIL import Image
+import torchvision
 
 class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self, path):
-        #train_labels = pd.read_csv("/Users/advaithg/Documents/ACMAI-Projects-W21/cassava-leaf-disease-classification/train.csv")
-        self.train_labels = pd.read_csv(path)
-
-    def __getitem__(self, index):
-        imgname = self.train_labels[index,0]
-        inputs = "/Users/advaithg/Documents/ACMAI-Projects-W21/cassava-leaf-disease-classification/train_images/" + imgname
-        img = Image.open(inputs)
-        label = self.train_labels[index,1]
-        return img, label
-
-    def __showitem__(self, index):
-        img, label = self.__getitem__(index)
-        plt.imshow(img[0].squeeze(), cmap = 'gray')
-        print(label)
+    def __init__(self, images_dir):
+        self.csv_file = pd.read_csv('train.csv')
+        self.images_dir = images_dir
 
     def __len__(self):
-        return 10000
+        return len(self.csv_file)
 
+    def __getitem__(self, index):
+
+        image_id = os.path.join(self.images_dir,
+                                self.csv_file.iloc[index, 0])
+        image = io.imread(image_id)
+        image = torchvision.transforms.ToTensor()
+        labels = self.csv_file.iloc[index, 1:]
+
+        return image, labels
+
+    def showitem(self, index):
+        img, label = self.getitem(index)
+        plt.imshow(img[0].squeeze(), cmap = 'gray')
+        print(label)
--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -18,3 +18,18 @@ class StartingNetwork(torch.nn.Module):
         x = self.fc(x)
         x = self.sigmoid(x)
         return x
+
+class ConvNet(torch.nn.Module):
+    def __init__(self, input_channels, output_dim):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.first = StartingNetwork(16*4*4, output_dim)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.reshape(x, (-1, 16 * 4 * 4))
+        x = self.first.forward(x)
+        return x
