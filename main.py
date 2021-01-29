a228c1afa7b8bd50a431cc8c0020462569af6351
Trevor <tr89on@gmail.com>
2021-01-28 21:46:53 -0800

    changed count for test

--- main.py
+++ main.py
@@ -30,12 +30,13 @@ def main():
     print("Batch size:", args.batch_size)
 
     # Initalize dataset and model. Then train the model!
-    count = 21397
+    # count = 21397
+    count = 64
     train_prop = 0.70
     path = './cassava-leaf-disease-classification/train.csv'
     data = np.genfromtxt(path, delimiter=',', dtype='str')
     train_dataset = StartingDataset(truth = data[1:int(count*train_prop), 1], images = data[1:int(count*train_prop), 0])
-    val_dataset = StartingDataset(truth = data[int(count*train_prop):, 1], images = data[int(count*train_prop):, 0]) # filler code, replace later
+    val_dataset = StartingDataset(truth = data[int(count*train_prop):count, 1], images = data[int(count*train_prop):count, 0]) # filler code, replace later
     model = StartingNetwork(3, 5)
     starting_train(
         train_dataset=train_dataset,
