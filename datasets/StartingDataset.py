8850f7fa7234f4324dda11b3f6ca3e535a9cc1cf
weihanhowardqu <78530053+weihanhowardqu@users.noreply.github.com>
2021-04-28 10:18:15 +0800

    Update StartingDataset.py
    Before splitting the datasets into training, testing, and validation sets

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -1,19 +1,23 @@
 import torch
+from torchvision.transforms import ToTensor, Lambda, transforms
+import pandas as pd
+import PIL
+from PIL import Image as PImage
+from os import listdir
 
-
-class StartingDataset(torch.utils.data.Dataset):
-    """
-    Dataset that contains 100000 3x224x224 black images (all zeros).
-    """
-
-    def __init__(self):
-        pass
-
-    def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
-
-        return inputs, label
-
+class StatementDataset(torch.utils.data.Dataset):#inherit from torch.utils.data.Dataset to make our life easier in dealing with Data
+    def __init__(self, statements, labels): #image_id, labels 
+        self.statements = statements
+        self.labels = labels 
     def __len__(self):
-        return 10000
+        return len(self.statements)
+    def __getitem__(self, index):  #retrieve items from our dataset 
+        path ='/Users/howard/Desktop/ACM-Project-Datasets/train_images/'
+        trans1 = transforms.ToTensor()        
+        statement = PImage.open(path+self.statements[index]) #read specific image
+        statement = trans1(statement)
+        label = self.labels[index]
+        return (statement,label) #return a tuple 
+
+image_info = pd.read_csv((r'/Users/howard/Desktop/ACM-Project-Datasets/train.csv'))
+image_dataset = StatementDataset(image_info.image_id,image_info.label)
