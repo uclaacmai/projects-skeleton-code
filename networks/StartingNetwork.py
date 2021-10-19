b007b0c1f9b94a2ff98a3041d306a8cfdcdc4159
JensonChoi <jenson.choi@gmail.com>
2021-10-18 18:45:37 -0700

    Implemented a simple CNN

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -9,12 +9,21 @@ class StartingNetwork(torch.nn.Module):
 
     def __init__(self):
         super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, stride=2)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, stride=2)
+        self.relu = nn.ReLU()
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(224 * 224 * 3, 1)
-        self.sigmoid = nn.Sigmoid()
+        self.fc1 = nn.Linear(56 * 56 * 16, 256)
+        self.fc2 = nn.Linear(256, 5)
 
     def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.pool1(x)
+        x = self.relu(self.conv2(x))
+        x = self.pool2(x)
         x = self.flatten(x)
-        x = self.fc(x)
-        x = self.sigmoid(x)
+        x = self.relu(self.fc1(x))
+        x = self.fc2(x)
         return x
