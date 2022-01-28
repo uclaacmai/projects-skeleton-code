b56379ed5bfc469f4361f12ca47a851bde134af4
Siddharth Nandy <sidd.nandy@gmail.com>
2022-01-27 19:43:14 -0800

    Created dataset class that stores image paths and labels and can resize images

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,19 +1,39 @@
+from ctypes import resize
 import torch
+from PIL import Image
 
+#TODO: Move constants to constants.py
+INPUT_WIDTH=200
+INPUT_HEIGHT=150
 
 class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, pictures, labels):
+        self.pictures = pictures
+        self.labels = labels
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        # Grab a single training example
+        picture = self.pictures[index]
+        # Load and resize the desired image
+        im = self.resizeImage(picture)
+        label = self.labels[index]
+        example = (im, label)
 
-        return inputs, label
+        return example
 
     def __len__(self):
-        return 10000
+        return len(self.pictures)
+
+    def resizeImage(self, im_path):
+        im = Image.open(im_path)
+        im = im.resize((INPUT_WIDTH, INPUT_HEIGHT))
+        # im.show()
+        return im
+
+if __name__ == "__main__":
+    datasetInstance = StartingDataset(["data/cassava-leaf-disease-classification/train_images/1000015157.jpg"], [0])
+    # datasetInstance.resizeImage(datasetInstance.pictures[0])
