35b286ab83a4192195c690b3a91d996a23c8b3b7
Siddharth Nandy <sidd.nandy@gmail.com>
2022-01-27 19:47:43 -0800

    Minor changes to main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 import constants
+import torch
 from data.StartingDataset import StartingDataset
 from networks.StartingNetwork import StartingNetwork
 from train_functions.starting_train import starting_train
@@ -13,16 +14,26 @@ def main():
     # TODO: Add GPU support. This line of code might be helpful.
     # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
     print("Epochs:", constants.EPOCHS)
     print("Batch size:", constants.BATCH_SIZE)
+
+    dimensions = [200, 150]
 
     # Initalize dataset and model. Then train the model!
     train_dataset = StartingDataset()
     val_dataset = StartingDataset()
-    model = StartingNetwork()
+    model = StartingNetwork(dimensions)
+    model = model.to(device)
+    
     starting_train(
         train_dataset=train_dataset,
         val_dataset=val_dataset,
+        dimensions=dimensions,
         model=model,
         hyperparameters=hyperparameters,
         n_eval=constants.N_EVAL,
