6835a8865276c25115ed73b1b6e5a4401cc4832c
Siddharth Nandy <sidd.nandy@gmail.com>
2022-01-27 20:02:49 -0800

    Create rudimentary convolutional layers in network class

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -2,19 +2,38 @@ import torch
 import torch.nn as nn
 
 
-class StartingNetwork(torch.nn.Module):
-    """
-    Basic logistic regression on 224x224x3 images.
-    """
-
-    def __init__(self):
+class StartingNetwork(nn.Module):
+    def __init__(self, dimensions):
+        # Call nn.Module's constructor
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.fc = nn.Linear(224 * 224 * 3, 1)
-        self.sigmoid = nn.Sigmoid()
+        # TODO: Hard-code dimensions/import from constants; no need to make it an argument
+        self.dimensions = dimensions
+        
+        # 150 x 200 x 3
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5, padding=2)
+
+        # 150 x 200 x 6
+        self.pool1 = nn.MaxPool2d(2, 2)
+
+        # 75 x 100 x 6
+        self.conv2 = nn.Conv2d(6, 8, kernel_size=5, padding=0)
+
+        # 71 x 96 x 8
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        # 35 x 48 x 8
+        # Define layers
+        self.fc1 = nn.Linear(35 * 48 * 8, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 5)
 
     def forward(self, x):
-        x = self.flatten(x)
-        x = self.fc(x)
-        x = self.sigmoid(x)
+        # Forward propagation
+        x = self.fc1(x)
+        x = F.relu(x)
+
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = self.fc3(x)
         return x
