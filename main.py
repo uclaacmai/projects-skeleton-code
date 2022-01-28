4414356255f7939a6dc7855ce9da90b68642d188
Siddharth Nandy <sidd.nandy@gmail.com>
2022-01-27 20:02:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,17 +7,12 @@ from networks.StartingNetwork import StartingNetwork
 from train_functions.starting_train import starting_train
 
 
+
 def main():
     # Get command line arguments
     hyperparameters = {"epochs": constants.EPOCHS, "batch_size": constants.BATCH_SIZE}
 
-    # TODO: Add GPU support. This line of code might be helpful.
-    # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    if torch.cuda.is_available():
-        device = torch.device("cuda")
-    else:
-        device = torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     print("Epochs:", constants.EPOCHS)
     print("Batch size:", constants.BATCH_SIZE)
