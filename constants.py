b6d058767124fdc51aa918f424f5c04097a7885d
Anand Gowda <acgowda@ucla.edu>
2022-01-27 20:08:42 -0800

    Updated StartingDataset.py

--- constants.py
+++ constants.py
@@ -1,3 +1,4 @@
 EPOCHS = 100
 BATCH_SIZE = 32
 N_EVAL = 100
+DATA = "../50whales/sauce"
--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,4 +1,8 @@
 import torch
+from PIL import Image
+from PIL import ImageOps
+import pandas as pd
+import constants
 
 
 class StartingDataset(torch.utils.data.Dataset):
@@ -6,14 +10,19 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, path):
+        self.path = path
+        self.images, self.labels = pd.read_csv(constants.DATA + "/train.csv")
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        image = Image.open(constants.DATA + self.path + self.images[index])
+        label = self.labels[index]
 
-        return inputs, label
+        image = image.resize(224, 448)
+        image = ImageOps.grayscale(image)
+
+        return image, label
+
 
     def __len__(self):
-        return 10000
+        return len(self.labels)
