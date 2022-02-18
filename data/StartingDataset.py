2ee99896423b0e9dfbc56f52483f921c4267468b
bkam002 <brandonkam02@gmail.com>
2022-02-17 19:37:22 -0800

    Data Augmentation Implementation

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -2,7 +2,8 @@ from ctypes import resize
 import torch
 import pandas as pd
 from PIL import Image
-from torchvision.transforms import transforms
+import torchvision.transforms as transforms
+import re
 
 INPUT_WIDTH=224
 INPUT_HEIGHT=224
@@ -20,9 +21,19 @@ class StartingDataset(torch.utils.data.Dataset):
         self.device = device
         #convert to list
 
-        for i in range(len(self.pictures)):
+        x = len(self.pictures)
+        for i in range(x):
             if(self.labels[i] != 3):
-                self.pictures.append(self.pictures[i][:-4] + "_r.jpg")
+                temp = self.pictures[i][:-4]
+                self.pictures.append(temp + "_r.jpg")
+                self.labels.append(self.labels[i])
+                
+                self.pictures.append(temp + "_gb.jpg")
+                self.labels.append(self.labels[i])
+
+        # possible transformations: flip, rotate, translation, blur, brightness/hue (dunno if we want to mess with that), 
+                
+        print(len(self.pictures))
 
         # if label is 1, 2, 4, 0: add transformations needed to balance data
         # append _something to the end of filename to specify transformation later
@@ -32,15 +43,14 @@ class StartingDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         # Grab a single training example
         picture = self.pictures[index]
-        picture.replace("_r","")
+        picture = re.sub(r'_[a-z]+',"",picture)
         # Load and resize the desired image
         im = self.resizeImage(self.path + "/cassava-leaf-disease-classification/train_images/" + picture)
         label = self.labels[index]
         trans = transforms.ToTensor()
         im = trans(im)
         if(self.pictures[index].endswith("_r.jpg")):
-            im = transforms.rotate(im, 45)
-        
+            im = transforms.functional.rotate(im, 45)
         # perform transformation if filename has _ in it
         
         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
