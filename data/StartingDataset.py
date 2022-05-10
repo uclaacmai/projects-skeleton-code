c6a9ac069b72124b538687c82bf63a4d52e98e61
Sumedha Goyal <sumedhagoyal@Sumedhas-MacBook-Pro.local>
2022-05-09 19:12:34 -0700

    added path var for the data

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -13,17 +13,19 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
     ###create this function
-    def __init__(self, isTrain):
+
+    def __init__(self, isTrain, datapath = 'cassava-leaf-disease-classification/'):
+        self.datapath = datapath
         if(isTrain):
-            self.csv_data = pandas.read_csv(constants.PATH_TO_DATA + 'train.csv').head(19257).to_numpy()
+            self.csv_data = pandas.read_csv(self.datapath + 'train.csv').head(19257).to_numpy()
         else:
-            self.csv_data = pandas.read_csv(constants.PATH_TO_DATA + 'train.csv').tail(2140).to_numpy()
+            self.csv_data = pandas.read_csv(self.datapath + 'train.csv').tail(2140).to_numpy()
     def __getitem__(self, index):
         ## do loading here
         image_name, label = self.csv_data[index]
 
         # save image
-        with Image.open(constants.PATH_TO_DATA+'/train_images/'+image_name) as inputs:
+        with Image.open(self.datapath+'/train_images/'+image_name) as inputs:
             inputs = torchvision.transforms.functional.resize(inputs, (224, 224))
             inputs = torchvision.transforms.ToTensor()(inputs)
             return inputs, label
